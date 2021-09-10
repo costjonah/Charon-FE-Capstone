9372ef0d982ca14e4f352e5df433656c73747b59
Chris Chung <chris.ch.987@gmail.com>
2021-09-09 21:19:06 -0400

    Add QA routes

--- server/models/questionsAndAnswers.js
+++ server/models/questionsAndAnswers.js
@@ -2,10 +2,84 @@ const axios = require('axios');
 const settings = require('./settings.js');
 
 module.exports = {
-  get: (req, res) => {
-    axios.get(settings.url + '/', settings.head)
-    .then(result => {
-      console.log(result);
-    })
-  }
-}
+  questions: (callback) => {
+    axios
+      .get(`${settings.url}/qa/questions`, settings.head)
+      .then((res) => {
+        callback(null, res);
+      })
+      .catch((err) => {
+        callback(err);
+      });
+  },
+  answers: (id = 1, callback) => {
+    axios
+      .get(`${settings.url}/qa/questions/${id}/answers`, settings.head)
+      .then((res) => {
+        callback(null, res);
+      })
+      .catch((err) => {
+        callback(err);
+      });
+  },
+  addQuestion: (callback) => {
+    axios
+      .post(`${settings.url}/qa/questions`, settings.head)
+      .then((res) => {
+        callback(null, res);
+      })
+      .catch((err) => {
+        callback(err);
+      });
+  },
+  addAnswer: (id, callback) => {
+    axios
+      .post(`${settings.url}/qa/questions/${id}/answers`, settings.head)
+      .then((res) => {
+        callback(null, res);
+      })
+      .catch((err) => {
+        callback(err);
+      });
+  },
+  markQuestionAsHelpful: (id, callback) => {
+    axios
+      .put(`${settings.url}/qa/questions/${id}/helpful`, settings.head)
+      .then((res) => {
+        callback(null, res);
+      })
+      .catch((err) => {
+        callback(err);
+      });
+  },
+  reportQuestion: (id, callback) => {
+    axios
+      .put(`${settings.url}/qa/questions/${id}/report`, settings.head)
+      .then((res) => {
+        callback(null, res);
+      })
+      .catch((err) => {
+        callback(err);
+      });
+  },
+  markAnswerAsHelpful: (id, callback) => {
+    axios
+      .put(`${settings.url}/qa/answers/${id}/helpful`, settings.head)
+      .then((res) => {
+        callback(null, res);
+      })
+      .catch((err) => {
+        callback(err);
+      });
+  },
+  reportAnswer: (id, callback) => {
+    axios
+      .put(`${settings.url}/qa/answers/${id}/report`, settings.head)
+      .then((res) => {
+        callback(null, res);
+      })
+      .catch((err) => {
+        callback(err);
+      });
+  },
+};
--- server/routes/questionsAndAnswers.js
+++ server/routes/questionsAndAnswers.js
@@ -1,7 +1,90 @@
-const models = require('../models/index')
+const models = require('../models/index');
 
 module.exports = (app) => {
-  app.get('/', (req, res) => {
-    models.questionsAndAnswers.get(req, res);
+  app.get('/qa/questions', (req, res) => {
+    models.questionsAndAnswers.questions((err, responseData) => {
+      if (err) {
+        console.error('Error: ', err);
+        res.status(500).end();
+      } else {
+        res.send(responseData.data);
+      }
+    });
   });
-}
+  app.get('/qa/questions/:question_id/answers', (req, res) => {
+    let id = req.params.question_id;
+    models.questionsAndAnswers.answers(id, (err, responseData) => {
+      if (err) {
+        console.error('Error: ', err);
+        res.status(500).end();
+      } else {
+        res.send(responseData.data);
+      }
+    });
+  });
+  app.post('/qa/questions', (req, res) => {
+    models.questionsAndAnswers.addQuestion((err, responseData) => {
+      if (err) {
+        console.error('Error: ', err);
+        res.status(500).end();
+      } else {
+        res.status(201).send(responseData.data);
+      }
+    });
+  });
+  app.post('/qa/questions/:question_id/answers', (req, res) => {
+    let id = req.params.question_id;
+    models.questionsAndAnswers.addAnswer(id, (err, responseData) => {
+      if (err) {
+        console.error('Error: ', err);
+        res.status(500).end();
+      } else {
+        res.status(201).send(responseData.data);
+      }
+    });
+  });
+  app.put('/qa/questions/:question_id/helpful', (req, res) => {
+    let id = req.params.question_id;
+    models.questionsAndAnswers.markQuestionAsHelpful(id, (err, responseData) => {
+      if (err) {
+        console.error('Error: ', err);
+        res.status(500).end();
+      } else {
+        res.status(204).send(responseData.data);
+      }
+    });
+  });
+  app.put('/qa/questions/:question_id/report', (req, res) => {
+    let id = req.params.question_id;
+    models.questionsAndAnswers.reportQuestion(id, (err, responseData) => {
+      if (err) {
+        console.error('Error: ', err);
+        res.status(500).end();
+      } else {
+        res.status(204).send(responseData.data);
+      }
+    });
+  });
+  app.put('/qa/answers/:answer_id/helpful', (req, res) => {
+    let id = req.params.answer_id;
+    models.questionsAndAnswers.markAnswerAsHelpful(id, (err, responseData) => {
+      if (err) {
+        console.error('Error: ', err);
+        res.status(500).end();
+      } else {
+        res.status(204).send(responseData.data);
+      }
+    });
+  });
+  app.put('/qa/answers/:answer_id/report', (req, res) => {
+    let id = req.params.answer_id;
+    models.questionsAndAnswers.reportAnswer(id, (err, responseData) => {
+      if (err) {
+        console.error('Error: ', err);
+        res.status(500).end();
+      } else {
+        res.status(204).send(responseData.data);
+      }
+    });
+  });
+};
