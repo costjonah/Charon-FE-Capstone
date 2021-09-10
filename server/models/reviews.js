0b1002df1717e60acc89d145efb81c9eea813fbf
Chris Chung <chris.ch.987@gmail.com>
2021-09-09 21:00:51 -0400

    Add Reviews routes

--- server/models/reviews.js
+++ server/models/reviews.js
@@ -2,10 +2,54 @@ const axios = require('axios');
 const settings = require('./settings.js');
 
 module.exports = {
-  get: (req, res) => {
-    axios.get(settings.url + '/', settings.head)
-    .then(result => {
-      console.log(result);
-    })
-  }
-}
+  list: (callback) => {
+    axios
+      .get(`${settings.url}/reviews`, settings.head)
+      .then((res) => {
+        callback(null, res);
+      })
+      .catch((err) => {
+        callback(err);
+      });
+  },
+  getMetadata: (callback) => {
+    axios
+      .get(`${settings.url}/reviews/meta`, settings.head)
+      .then((res) => {
+        callback(null, res);
+      })
+      .catch((err) => {
+        callback(err);
+      });
+  },
+  add: (callback) => {
+    axios
+      .post(`${settings.url}/reviews`, settings.head)
+      .then((res) => {
+        callback(null, res);
+      })
+      .catch((err) => {
+        callback(err);
+      });
+  },
+  markAsHelpful: (id = 1, callback) => {
+    axios
+      .put(`${settings.url}/reviews/${id}/helpful`, settings.head)
+      .then((res) => {
+        callback(null, res);
+      })
+      .catch((err) => {
+        callback(err);
+      });
+  },
+  report: (id = 1, callback) => {
+    axios
+      .put(`${settings.url}/reviews/${id}/report`, settings.head)
+      .then((res) => {
+        callback(null, res);
+      })
+      .catch((err) => {
+        callback(err);
+      });
+  },
+};
--- server/routes/reviews.js
+++ server/routes/reviews.js
@@ -1,7 +1,57 @@
 const models = require('../models/index')
 
 module.exports = (app) => {
-  app.get('/', (req, res) => {
-    models.reviews.get(req, res);
+  app.get('/reviews', (req, res) => {
+    models.reviews.get((err, responseData) => {
+      if (err) {
+        console.error('Error: ', err);
+        res.status(500).end();
+      } else {
+        res.send(responseData.data);
+      }
+    });
+  });
+  app.get('/reviews/meta', (req, res) => {
+    models.reviews.getById((err, responseData) => {
+      if (err) {
+        console.error('Error: ', err);
+        res.status(500).end();
+      } else {
+        res.send(responseData.data);
+      }
+    });
+  });
+  app.post('/reviews', (req, res) => {
+    let id = req.params.product_id;
+    models.reviews.getStylesById(id, (err, responseData) => {
+      if (err) {
+        console.error('Error: ', err);
+        res.status(500).end();
+      } else {
+        res.status(201).send(responseData.data);
+      }
+    });
+  });
+  app.put('/reviews/:review_id/helpful', (req, res) => {
+    let id = req.params.product_id;
+    models.reviews.getRelatedById(id, (err, responseData) => {
+      if (err) {
+        console.error('Error: ', err);
+        res.status(500).end();
+      } else {
+        res.status(204).send(responseData.data);
+      }
+    });
+  });
+  app.put('/reviews/:review_id/report', (req, res) => {
+    let id = req.params.product_id;
+    models.reviews.getRelatedById(id, (err, responseData) => {
+      if (err) {
+        console.error('Error: ', err);
+        res.status(500).end();
+      } else {
+        res.status(204).send(responseData.data);
+      }
+    });
   });
 }
