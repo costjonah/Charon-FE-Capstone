7b80826d41f0456a96256d43c7498ba74bf04566
Chris Chung <chris.ch.987@gmail.com>
2021-09-09 20:41:02 -0400

    Add Product routes

--- server/models/products.js
+++ server/models/products.js
@@ -2,11 +2,44 @@ const axios = require('axios');
 const settings = require('./settings.js');
 
 module.exports = {
-  get: (req, res) => {
-    axios.get(settings.url + '/products', settings.head)
-    .then(result => {
-      console.log(result.data);
-    })
-    .catch(err => { console.error('Error: ', err); });
-  }
-}
+  get: (callback) => {
+    axios
+      .get(`${settings.url}/products`, settings.head)
+      .then((res) => {
+        callback(null, res);
+      })
+      .catch((err) => {
+        callback(err);
+      });
+  },
+  getById: (id = 1, callback) => {
+    axios
+      .get(`${settings.url}/products/${id}`, settings.head)
+      .then((res) => {
+        callback(null, res);
+      })
+      .catch((err) => {
+        callback(err);
+      });
+  },
+  getStylesById: (id = 1, callback) => {
+    axios
+      .get(`${settings.url}/products/${id}/styles`, settings.head)
+      .then((res) => {
+        callback(null, res);
+      })
+      .catch((err) => {
+        callback(err);
+      });
+  },
+  getRelatedById: (id = 1, callback) => {
+    axios
+      .get(`${settings.url}/products/${id}/related`, settings.head)
+      .then((res) => {
+        callback(null, res);
+      })
+      .catch((err) => {
+        callback(err);
+      });
+  },
+};
--- server/routes/products.js
+++ server/routes/products.js
@@ -1,8 +1,47 @@
-const models = require('../models/index')
+const models = require('../models/index');
 
 module.exports = (app) => {
   app.get('/products', (req, res) => {
-    models.products.get(req, res);
-    res.end();
+    models.products.get((err, responseData) => {
+      if (err) {
+        console.error('Error: ', err);
+        res.status(500).end();
+      } else {
+        res.send(responseData.data);
+      }
+    });
   });
-}
+  app.get('/products/:product_id', (req, res) => {
+    let id = req.params.product_id;
+    models.products.getById(id, (err, responseData) => {
+      if (err) {
+        console.error('Error: ', err);
+        res.status(500).end();
+      } else {
+        res.send(responseData.data);
+      }
+    });
+  });
+  app.get('/products/:product_id/styles', (req, res) => {
+    let id = req.params.product_id;
+    models.products.getStylesById(id, (err, responseData) => {
+      if (err) {
+        console.error('Error: ', err);
+        res.status(500).end();
+      } else {
+        res.send(responseData.data);
+      }
+    });
+  });
+  app.get('/products/:product_id/related', (req, res) => {
+    let id = req.params.product_id;
+    models.products.getRelatedById(id, (err, responseData) => {
+      if (err) {
+        console.error('Error: ', err);
+        res.status(500).end();
+      } else {
+        res.send(responseData.data);
+      }
+    });
+  });
+};
