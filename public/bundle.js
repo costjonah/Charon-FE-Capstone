b98c9e37186dfceda5212b409a69009032610d71
Jonah Cost <costjonah@gmail.com>
2021-09-12 17:09:48 -0400

    Created webpack file. Created initial structure for main react class component. Created folder for main component(overview widgets) files.

---
+++ public/bundle.js
@@ -0,0 +1,32 @@
+/*
+ * ATTENTION: The "eval" devtool has been used (maybe by default in mode: "development").
+ * This devtool is neither made for production nor for readable output files.
+ * It uses "eval()" calls to create a separate source file in the browser devtools.
+ * If you are trying to read the output file, select a different devtool (https://webpack.js.org/configuration/devtool/)
+ * or disable the default devtool with "devtool: false".
+ * If you are looking for production-ready output files, see mode: "production" (https://webpack.js.org/configuration/mode/).
+ */
+/******/ (() => { // webpackBootstrap
+/******/ 	var __webpack_modules__ = ({
+
+/***/ "./client/src/index.js":
+/*!*****************************!*\
+  !*** ./client/src/index.js ***!
+  \*****************************/
+/***/ (() => {
+
+eval("throw new Error(\"Module build failed (from ./node_modules/babel-loader/lib/index.js):\\nSyntaxError: /Users/JonahC/HR/SEI/Senior/Charon-FE-Capstone/client/src/index.js: Support for the experimental syntax 'jsx' isn't currently enabled (5:17):\\n\\n\\u001b[0m \\u001b[90m 3 |\\u001b[39m \\u001b[36mimport\\u001b[39m \\u001b[33mApp\\u001b[39m \\u001b[36mfrom\\u001b[39m \\u001b[32m'./components/App.jsx'\\u001b[39m\\u001b[33m;\\u001b[39m\\u001b[0m\\n\\u001b[0m \\u001b[90m 4 |\\u001b[39m\\u001b[0m\\n\\u001b[0m\\u001b[31m\\u001b[1m>\\u001b[22m\\u001b[39m\\u001b[90m 5 |\\u001b[39m \\u001b[33mReactDOM\\u001b[39m\\u001b[33m.\\u001b[39mrender(\\u001b[33m<\\u001b[39m\\u001b[33mApp\\u001b[39m \\u001b[33m/\\u001b[39m\\u001b[33m>\\u001b[39m\\u001b[33m,\\u001b[39m document\\u001b[33m.\\u001b[39mgetElementById(\\u001b[32m'app'\\u001b[39m))\\u001b[33m;\\u001b[39m\\u001b[0m\\n\\u001b[0m \\u001b[90m   |\\u001b[39m                 \\u001b[31m\\u001b[1m^\\u001b[22m\\u001b[39m\\u001b[0m\\n\\nAdd @babel/preset-react (https://git.io/JfeDR) to the 'presets' section of your Babel config to enable transformation.\\nIf you want to leave it as-is, add @babel/plugin-syntax-jsx (https://git.io/vb4yA) to the 'plugins' section to enable parsing.\\n    at Parser._raise (/Users/JonahC/HR/SEI/Senior/Charon-FE-Capstone/node_modules/@babel/parser/lib/index.js:807:17)\\n    at Parser.raiseWithData (/Users/JonahC/HR/SEI/Senior/Charon-FE-Capstone/node_modules/@babel/parser/lib/index.js:800:17)\\n    at Parser.expectOnePlugin (/Users/JonahC/HR/SEI/Senior/Charon-FE-Capstone/node_modules/@babel/parser/lib/index.js:3281:18)\\n    at Parser.parseExprAtom (/Users/JonahC/HR/SEI/Senior/Charon-FE-Capstone/node_modules/@babel/parser/lib/index.js:11544:20)\\n    at Parser.parseExprSubscripts (/Users/JonahC/HR/SEI/Senior/Charon-FE-Capstone/node_modules/@babel/parser/lib/index.js:11119:23)\\n    at Parser.parseUpdate (/Users/JonahC/HR/SEI/Senior/Charon-FE-Capstone/node_modules/@babel/parser/lib/index.js:11099:21)\\n    at Parser.parseMaybeUnary (/Users/JonahC/HR/SEI/Senior/Charon-FE-Capstone/node_modules/@babel/parser/lib/index.js:11077:23)\\n    at Parser.parseMaybeUnaryOrPrivate (/Users/JonahC/HR/SEI/Senior/Charon-FE-Capstone/node_modules/@babel/parser/lib/index.js:10900:77)\\n    at Parser.parseExprOps (/Users/JonahC/HR/SEI/Senior/Charon-FE-Capstone/node_modules/@babel/parser/lib/index.js:10907:23)\\n    at Parser.parseMaybeConditional (/Users/JonahC/HR/SEI/Senior/Charon-FE-Capstone/node_modules/@babel/parser/lib/index.js:10877:23)\");\n\n//# sourceURL=webpack://Charon-FE-Capstone/./client/src/index.js?");
+
+/***/ })
+
+/******/ 	});
+/************************************************************************/
+/******/ 	
+/******/ 	// startup
+/******/ 	// Load entry module and return exports
+/******/ 	// This entry module doesn't tell about it's top-level declarations so it can't be inlined
+/******/ 	var __webpack_exports__ = {};
+/******/ 	__webpack_modules__["./client/src/index.js"]();
+/******/ 	
+/******/ })()
+;
---
+++ webpack.config.js
@@ -0,0 +1,29 @@
+const path = require('path');
+
+const SRC_DIR = path.join(__dirname, '/client/src');
+const DIST_DIR = path.join(__dirname, '/client/dist');
+
+module.exports = {
+  entry: path.join(SRC_DIR, 'index.js'),
+  output: {
+    path: DIST_DIR,
+    filename: 'bundle.js'
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        loader: 'babel-loader',
+        options: {
+          presets: ['@babel/preset-env', '@babel/preset-react'],
+        },
+      }
+    ]
+  },
+  mode: 'development',
+  resolve: {
+    extensions: ['.js', '.jsx']
+  }
+};
+
