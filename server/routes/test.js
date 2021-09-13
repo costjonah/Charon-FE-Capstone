764d00b8ac23a1d7612ddd688ef2db39c03c3c4e
EndlessDavidDu <trickyheight830@gmail.com>
2021-09-13 13:04:34 -0400

    Add test file

---
+++ server/routes/test.js
@@ -0,0 +1,24 @@
+const productList = require('../data/productList');
+
+module.exports = (app) => {
+
+    app.get("/api/test/products", (req, res) => {
+        return res.json(productList);
+    });
+
+    app.get("/api/test/products/:id", (req, res) => {
+        let product = null;
+        for (const p of productList.productList) {
+            if (p.id == req.params.id) {
+                product = p;
+            }
+        }
+
+        return res.json({
+            code: 200,
+            msg: "ok",
+            product: product
+        });
+    });
+
+};
