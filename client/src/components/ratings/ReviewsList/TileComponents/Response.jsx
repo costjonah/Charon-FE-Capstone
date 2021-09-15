d5bc79bc9f5745b45f1adead74a8a5076949e10b
Chris Chung <chris.ch.987@gmail.com>
2021-09-14 21:54:21 -0400

    Add border to visually distinguish response

--- client/src/components/ratings/ReviewsList/TileComponents/Response.jsx
+++ client/src/components/ratings/ReviewsList/TileComponents/Response.jsx
@@ -2,7 +2,11 @@ import React from 'react';
 
 const Response = (props) => {
   if (props.response && props.response.length > 0) {
-    return <div>Response from seller: {props.response}</div>;
+    return (
+      <div style={{ borderStyle: 'solid' }}>
+        Response from seller: {props.response}
+      </div>
+    );
   }
   return null;
 };
