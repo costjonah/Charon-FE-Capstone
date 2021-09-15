1e6315267d0c844957461b51ab561e28f772ea46
Chris Chung <chris.ch.987@gmail.com>
2021-09-14 22:12:53 -0400

    Outline Helpfulness component

--- client/src/components/ratings/ReviewsList/TileComponents/Helpfulness.jsx
+++ client/src/components/ratings/ReviewsList/TileComponents/Helpfulness.jsx
@@ -1,8 +1,18 @@
 import React from 'react';
 
 const Helpfulness = (props) => {
-  //props.helpfulness
-  return null;
+  const doNothing = () => {
+    console.log('clicked this!');
+  };
+  return (
+    <div>
+      <div>Was this review helpful?</div>
+      <div>
+        <div onClick={doNothing}>Yes ({1})</div>
+        <div onClick={doNothing}>No ({2})</div>
+      </div>
+    </div>
+  );
 };
 
 export default Helpfulness;
