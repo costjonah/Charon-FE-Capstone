632e8abef81d35abb0c675f3a376c0f3c250d80c
Chris Chung <chris.ch.987@gmail.com>
2021-09-17 12:06:58 -0400

    Layout Ratings Breakdown

--- client/src/components/ratings/RatingsBreakdown/BreakdownComponents/Breakdown.jsx
+++ client/src/components/ratings/RatingsBreakdown/BreakdownComponents/Breakdown.jsx
@@ -1,7 +1,38 @@
 import React from 'react';
 
 const Breakdown = (props) => {
-  return <div>Breakdown</div>;
+  return (
+    <React.Fragment>
+      <div>Breakdown</div>
+      <div>
+        <span>5 stars</span>
+        <div
+          className='bar'
+          style={{ borderStyle: 'solid', height: '10px' }}
+        ></div>
+        <span>4 stars</span>
+        <div
+          className='bar'
+          style={{ borderStyle: 'solid', height: '10px' }}
+        ></div>
+        <span>3 stars</span>
+        <div
+          className='bar'
+          style={{ borderStyle: 'solid', height: '10px' }}
+        ></div>
+        <span>2 stars</span>
+        <div
+          className='bar'
+          style={{ borderStyle: 'solid', height: '10px' }}
+        ></div>
+        <span>1 star</span>
+        <div
+          className='bar'
+          style={{ borderStyle: 'solid', height: '10px' }}
+        ></div>
+      </div>
+    </React.Fragment>
+  );
 };
 
 export default Breakdown;
