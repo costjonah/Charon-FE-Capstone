245bd1b3c76ad9695781b2528898b04fef5bf434
Chris Chung <chris.ch.987@gmail.com>
2021-09-19 23:39:52 -0400

    Update sort searchbar to match rest of page

--- client/src/components/ratings/SortOptions/SortOptions.jsx
+++ client/src/components/ratings/SortOptions/SortOptions.jsx
@@ -1,15 +1,21 @@
 import React from 'react';
+import Select from 'react-select';
 
 class SortOptions extends React.Component {
   constructor(props) {
     super(props);
     this.state = {};
     this.handleChange = this.handleChange.bind(this);
+    this.options = [
+      { value: 'Relevant', label: 'Relevant' },
+      { value: 'Helpful', label: 'Helpful' },
+      { value: 'Newest', label: 'Newest' },
+    ];
   }
 
   handleChange(e) {
-    this.props.sort(e.target.value);
-    console.log('changed to ', e.target.value);
+    this.props.sort(e.value);
+    console.log('changed to ', e.value);
   }
 
   render() {
@@ -17,11 +23,12 @@ class SortOptions extends React.Component {
       <form>
         <label>
           Sort by:
-          <select value={this.props.value} onChange={this.handleChange}>
-            <option value='Relevant'>Relevant</option>
-            <option value='Helpful'>Helpful</option>
-            <option value='Newest'>Newest</option>
-          </select>
+          <Select
+            placeholder='Relevant'
+            options={this.options}
+            onChange={this.handleChange}
+            id='select'
+          />
         </label>
       </form>
     );
