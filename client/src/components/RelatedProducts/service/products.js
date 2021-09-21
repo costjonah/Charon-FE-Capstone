44ea35267249639b562e4cd0724794e97107dcfa
Jonah Cost <costjonah@gmail.com>
2021-09-21 13:43:01 -0400

    Charon full app, initial commit.

--- client/src/components/RelatedProducts/service/products.js
+++ client/src/components/RelatedProducts/service/products.js
@@ -1,20 +1,20 @@
-const BASE_URL_PREFFIX = ''
+const BASE_URL_PREFFIX = "";
 
 export const fetchProductList = async () => {
-  const res = await fetch(`${BASE_URL_PREFFIX}/products`)
-  const data = await res.json()
+  const res = await fetch(`${BASE_URL_PREFFIX}/products`);
+  const data = await res.json();
   //console.log(data);
-  return data
-}
+  return data;
+};
 
 export const fetchProduct = async (id) => {
-  const res = await fetch(`${BASE_URL_PREFFIX}/products/${id}`)
-  const data = await res.json()
-  return data
-}
+  const res = await fetch(`${BASE_URL_PREFFIX}/products/${id}`);
+  const data = await res.json();
+  return data;
+};
 
 export const fetchCurrentProduct = async () => {
-  const res = await fetch(`${BASE_URL_PREFFIX}/currentProduct`)
-  const data = await res.json()
-  return data
-}
+  const res = await fetch(`${BASE_URL_PREFFIX}/currentProduct`);
+  const data = await res.json();
+  return data;
+};
