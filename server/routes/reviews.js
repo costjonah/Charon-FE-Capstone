ab38f5978a5d48fa7fe06f5101ac6b8236d750a8
EndlessDavidDu <trickyheight830@gmail.com>
2021-09-22 10:12:39 -0400

    make changes

--- server/routes/reviews.js
+++ server/routes/reviews.js
@@ -1,36 +1,36 @@
-const models = require('../models/index');
+const models = require("../models/index");
 
 module.exports = (app) => {
-  app.get('/reviews', (req, res) => {
+  app.get("/reviews", (req, res) => {
     let queryParams = {
       page: req.query.page || 1,
       count: req.query.count || 5,
-      sort: req.query.sort || 'newest',
+      sort: req.query.sort || "newest",
       id: req.query.product_id,
     };
     models.reviews.list(queryParams, (err, responseData) => {
       if (err) {
-        console.error('Error: ', err);
-        res.status(500).end();
+        res.status(500).send(err);
       } else {
+        console.log(responseData);
         res.send(responseData.data);
       }
     });
   });
-  app.get('/reviews/meta', (req, res) => {
+  app.get("/reviews/meta", (req, res) => {
     let queryData = {
       id: req.query.product_id,
     };
     models.reviews.getMetadata(queryData, (err, responseData) => {
       if (err) {
-        console.error('Error: ', err);
+        console.error("Error: ", err);
         res.status(500).end();
       } else {
         res.send(responseData.data);
       }
     });
   });
-  app.post('/reviews', (req, res) => {
+  app.post("/reviews", (req, res) => {
     let bodyData = {
       product_id: req.body.product_id,
       rating: req.body.rating,
@@ -44,32 +44,32 @@ module.exports = (app) => {
     };
     models.reviews.add(bodyData, (err, responseData) => {
       if (err) {
-        console.error('Error: ', err.response.status);
-        console.error('Error: ', err.response.data);
+        console.error("Error: ", err.response.status);
+        console.error("Error: ", err.response.data);
         res.status(500).end();
       } else {
         res.status(201).send(responseData.data);
       }
     });
   });
-  app.put('/reviews/:review_id/helpful', (req, res) => {
+  app.put("/reviews/:review_id/helpful", (req, res) => {
     let id = req.params.review_id;
     models.reviews.markAsHelpful(id, (err, responseData) => {
       if (err) {
-        console.error('Error: ', err.response.status);
-        console.error('Error: ', err.response.data);
+        console.error("Error: ", err.response.status);
+        console.error("Error: ", err.response.data);
         res.status(500).end();
       } else {
         res.status(204).send(responseData.data);
       }
     });
   });
-  app.put('/reviews/:review_id/report', (req, res) => {
+  app.put("/reviews/:review_id/report", (req, res) => {
     let id = req.params.review_id;
     models.reviews.report(id, (err, responseData) => {
       if (err) {
-        console.error('Error: ', err.response.status);
-        console.error('Error: ', err.response.data);
+        console.error("Error: ", err.response.status);
+        console.error("Error: ", err.response.data);
         res.status(500).end();
       } else {
         res.status(204).send(responseData.data);
